a9fe7a3f244c3915bd59207afcf5318cacd7b7b6
radut <radut90@mail.ru>
2019-12-17 22:04:50 +0300

    Create project structure

--- src/main/java/com/csvparser/Application.java
+++ src/main/java/com/csvparser/Application.java
@@ -2,10 +2,15 @@ package com.csvparser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext(
+                        "/resources/META-INF/spring/app-context.xml");
         SpringApplication.run(Application.class);
     }
 }
