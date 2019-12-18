d20077f7ff3e5ecb512fa6b4868b9ea5d4239849
radut <radut90@mail.ru>
2019-12-18 21:17:15 +0300

    Configure connect MySql DB

--- src/main/java/com/csvparser/controller/CsvController.java
+++ src/main/java/com/csvparser/controller/CsvController.java
@@ -1,4 +1,15 @@
 package com.csvparser.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("csv-controller")
 public class CsvController {
+
+    @GetMapping(value = {"/", ""})
+    public String hello(){
+        System.out.println("Hello");
+        return "hello";
+    }
 }
--- src/main/java/com/csvparser/model/Csv.java
+++ src/main/java/com/csvparser/model/Csv.java
@@ -1,9 +1,35 @@
 package com.csvparser.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Csv {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    long id;
+    private String a;
 
+    private String b;
 
+    private String c;
+
+    private String d;
+
+    private String e;
+
+    private String f;
+
+    private String h;
+
+    private String i;
+
+    private String j;
 }
